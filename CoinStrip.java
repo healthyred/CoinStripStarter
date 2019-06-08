8051731fa24ed46dd2c739f55d58abe937894517
jian13579 <jhl3@williams.edu>
2019-06-08 00:14:27 -0400

    new

---
+++ CoinStrip.java
@@ -0,0 +1,97 @@
+///*Coinstrip Starter Code*/
+//
+//package edu.williams.cs;
+
+class CoinStrip{
+
+   /**
+    * Generates a random game with at least 3 coins.
+    */
+   public CoinStrip() {
+
+   }
+
+   /**
+    * Returns the number of coins in the game.
+    */
+   public int numCoins() {
+       return -1;
+
+   }
+
+   /**
+    * Returns true if the game has been won, otherwise false.
+    */
+   public boolean gameWon() {
+       return false;
+   }
+
+
+   /**
+    * Checks that a move is valid, and if so, updates the
+    * game board and returns true.  If a move is invalid,
+    * does not update the game board and returns false.
+    * Ensure that the player number changes in move() else
+    * points may be lost.
+    * @param the number of the coin
+    * @param the destination board position
+    */
+   public boolean move(int coin, int position) {
+       return false;
+   }
+
+   /**
+    * Returns the current player.
+    */
+   public int currentPlayer() {
+       return -1;
+
+   }
+
+   /**
+    * Updates the game state to the next player.
+    */
+   public void nextPlayer() {
+
+   }
+
+   /**
+    * Returns the last player.
+    */
+   public int lastPlayer() {
+       return -1;
+
+   }
+
+   /**
+    * Returns the position of the coin on the board.
+    * @param the coin number start count from 0.
+    */
+   public int getCoin(int coin) {
+       return -1;
+
+   }
+
+   /**
+    * Returns the board length.
+    */
+   public int getBoardLength(){
+       return -1;
+   }
+   /**
+    * Sets a coin on any index of the board.
+    * @param the index of the board
+    */
+   public void setCoin(int index){
+
+   }
+
+   /**
+    * Deletes all coins off the board.
+    */
+   public void clearBoard(){
+
+   }
+
+
+}
